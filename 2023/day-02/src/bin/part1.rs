7b3f669d10b47aed8f86d88ca5f6faddf84e1b0d
Donovan Parks <donovan.parks@gmail.com>
2023-12-03 13:50:22 -0800

    Using anywho to clean-up error handling.

--- 2023/day-02/src/bin/part1.rs
+++ 2023/day-02/src/bin/part1.rs
@@ -1,27 +1,34 @@
+use anyhow::{Context, Result};
+
 const MAX_RED: u32 = 12;
 const MAX_GREEN: u32 = 13;
 const MAX_BLUE: u32 = 14;
 
-fn main() {
+fn main() -> Result<()> {
     let input = include_str!("./input.txt");
-    let answer = process(input);
+    let answer = process(input)?;
     println!("answer: {answer}");
+
+    Ok(())
 }
 
-pub fn process(input: &str) -> String {
+pub fn process(input: &str) -> Result<String> {
     let mut answer = 0;
     for game in input.lines() {
-        let (game_data, cube_reps) = game.split_once(':').expect("valid game data");
+        let (game_data, cube_reps) = game.split_once(':').context("Invalid input data.")?;
 
         let mut valid_game = true;
         for cube_stats in cube_reps.split(';') {
             for cube_stat in cube_stats.split(',') {
-                let (num_cubes, color) = cube_stat.trim().split_once(' ').expect("valid cube data");
+                let (num_cubes, color) = cube_stat
+                    .trim()
+                    .split_once(' ')
+                    .context("Invalid cube data.")?;
 
                 let valid = match color {
-                    "red" => num_cubes.parse::<u32>().expect("valid cube number") <= MAX_RED,
-                    "green" => num_cubes.parse::<u32>().expect("valid cube number") <= MAX_GREEN,
-                    "blue" => num_cubes.parse::<u32>().expect("valid cube number") <= MAX_BLUE,
+                    "red" => num_cubes.parse::<u32>()? <= MAX_RED,
+                    "green" => num_cubes.parse::<u32>()? <= MAX_GREEN,
+                    "blue" => num_cubes.parse::<u32>()? <= MAX_BLUE,
                     _ => false,
                 };
 
@@ -37,12 +44,12 @@ pub fn process(input: &str) -> String {
         }
 
         if valid_game {
-            let (_game, game_num) = game_data.split_once(' ').expect("valid game data");
-            answer += game_num.parse::<u32>().expect("valid game number");
+            let (_game, game_num) = game_data.split_once(' ').context("Invalid game data.")?;
+            answer += game_num.parse::<u32>()?;
         }
     }
 
-    answer.to_string()
+    Ok(answer.to_string())
 }
 
 #[cfg(test)]
@@ -52,7 +59,7 @@ mod tests {
     #[test]
     fn test_answer() {
         let input = include_str!("./test.txt");
-        let answer = process(input);
+        let answer = process(input).unwrap();
         assert_eq!(answer, "8");
     }
 }
--- 2023/day-02/src/bin/part2.rs
+++ 2023/day-02/src/bin/part2.rs
@@ -1,22 +1,29 @@
-fn main() {
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
     let input = include_str!("./input.txt");
-    let answer = process(input);
+    let answer = process(input)?;
     println!("answer: {answer}");
+
+    Ok(())
 }
 
-pub fn process(input: &str) -> String {
+pub fn process(input: &str) -> Result<String> {
     let mut answer = 0;
     for game in input.lines() {
-        let (_game_data, cube_reps) = game.split_once(':').expect("valid game data");
+        let (_game_data, cube_reps) = game.split_once(':').context("valid game data")?;
 
         let mut min_red = 0;
         let mut min_green = 0;
         let mut min_blue = 0;
         for cube_stats in cube_reps.split(';') {
             for cube_stat in cube_stats.split(',') {
-                let (num_cubes, color) = cube_stat.trim().split_once(' ').expect("valid cube data");
+                let (num_cubes, color) = cube_stat
+                    .trim()
+                    .split_once(' ')
+                    .context("valid cube data")?;
 
-                let num_cubes = num_cubes.parse::<u32>().expect("valid cube number");
+                let num_cubes = num_cubes.parse::<u32>()?;
                 if color == "red" && num_cubes >= min_red {
                     min_red = num_cubes
                 } else if color == "green" && num_cubes >= min_green {
@@ -30,7 +37,7 @@ pub fn process(input: &str) -> String {
         answer += min_red * min_green * min_blue;
     }
 
-    answer.to_string()
+    Ok(answer.to_string())
 }
 
 #[cfg(test)]
@@ -40,7 +47,7 @@ mod tests {
     #[test]
     fn test_answer() {
         let input = include_str!("./test.txt");
-        let answer = process(input);
+        let answer = process(input).unwrap();
         assert_eq!(answer, "2286");
     }
 }
